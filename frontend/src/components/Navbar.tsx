bbb897a5d091b5b94e7aa0439874a94c6aa19f74
Jaime Lay <jaimelay1997@gmail.com>
2024-03-12 22:33:25 -0300

    chore: add responsive navbar header

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,11 +1,17 @@
-import Link from "next/link";
+"use client"
+
+import Link, { LinkProps } from "next/link";
 import { Button } from '@/components/ui/button'
+import {
+    Sheet,
+    SheetContent,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
 const data = [
-    {
-        title: "Sobre",
-        href: "/about",
-    },
     {
         title: "Área de Estudos",
         href: "/courses",
@@ -20,41 +26,126 @@ const data = [
     },
 ];
 
+const logoTitle = "TCC";
+
 export default function Navbar() {
+    const [openSidebar, setOpenSidebar] = useState(false);
+
     return (
-        <header className="flex items-center justify-between h-20 p-5">
-            <div className="">
-                <Link href="/" className="h-8">TCC</Link>
+        <header className="flex items-center justify-between w-full px-4 md:px-6 py-4 md:py-8">
+            <Sheet open={openSidebar} onOpenChange={setOpenSidebar}>
+                <SheetTrigger asChild>
+                    <Button
+                        variant="ghost"
+                        className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
+                    >
+                        <svg
+                            strokeWidth="1.5"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                        >
+                            <path
+                            d="M3 5H11"
+                            stroke="currentColor"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            ></path>
+                            <path
+                            d="M3 12H16"
+                            stroke="currentColor"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            ></path>
+                            <path
+                            d="M3 19H21"
+                            stroke="currentColor"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            ></path>
+                        </svg>
+                        <span className="sr-only">Toggle SidebarMenu</span>
+                    </Button>
+                </SheetTrigger>
+                <SheetContent side="left" className="pr-0">
+                    <div className="flex flex-col">
+                        <Link href="/" className=" items-center gap-2 mb-10 text-3xl font-bold">{logoTitle}</Link>
+
+                        <nav className="flex flex-col gap-6 text-sm font-medium">
+                            {data.map((link, id) => (
+                                <SidebarLink
+                                    key={id}
+                                    href={link.href}
+                                    className="hover:underline font-bold text-base mr-5"
+                                    onOpenChange={setOpenSidebar}
+                                >
+                                    {link.title}
+                                </SidebarLink>
+                            ))}
+                        </nav>
+                    </div>
+
+                </SheetContent>
+            </Sheet>
+
+            <div className="hidden md:flex flex-row">
+                <Link href="/" className=" items-center gap-2 mr-10 text-3xl font-bold">{logoTitle}</Link>
+
+                <nav className="m-auto items-center gap-6 text-sm font-medium">
+                    {data.map((link, id) => (
+                        <Link
+                            key={id}
+                            href={link.href}
+                            className="hover:underline font-bold text-base mr-5"
+                        >
+                            {link.title}
+                        </Link>
+                    ))}
+                </nav>
             </div>
 
-            <nav className="">
-                <ul className="flex items-center gap-x-8">
-                    {data.map((link, id) => (
-                        <li key={id}>
-                            <Link
-                                href={link.href}
-                                className="font-semibold px-3 py-2.5 dark:text-white text-zinc-600 dark:hover:text-primary-color hover:text-indigo-500 hover:underline duration-300 text-base"
-                            >
-                                {link.title}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
+            <div className="flex items-center gap-4">
+                <Link href="/account/login">
+                    <Button variant="default">Fazer Login</Button>    
+                </Link>
 
-            <div className="flex flex-row">
-                <div className="mr-3">
-                    <Link href="/account/login">
-                        <Button variant="default">Fazer Login</Button>    
-                    </Link>
-                </div>
-
-                <div>
-                    <Link href="/account/signup">
-                        <Button variant="secondary">Cadastrar</Button>
-                    </Link>
-                </div>
+                <Link href="/account/signup">
+                    <Button variant="secondary">Cadastrar</Button>
+                </Link>
             </div>
         </header>
     );
+}
+
+interface SidebarLinkProps extends LinkProps {
+    onOpenChange?: (open: boolean) => void
+    children: React.ReactNode
+    className?: string
+}
+
+function SidebarLink({
+    href,
+    onOpenChange,
+    className,
+    children,
+    ...props
+}: SidebarLinkProps) {
+    const router = useRouter()
+    return (
+        <Link
+            href={href}
+            onClick={() => {
+                router.push(href.toString())
+                onOpenChange?.(false)
+            }}
+            className={cn(className)}
+            {...props}
+        >
+            {children}
+        </Link>
+    )
 }
