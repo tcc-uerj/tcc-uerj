7a74ab29ba7699c3a07b5c3ce15c19893695b175
ows.lucas <ldunhao@hotmail.com>
2024-05-17 21:45:31 -0300

    chore: ajustado retorno do login

--- backend/src/infra/repositories/user/user.repository.ts
+++ backend/src/infra/repositories/user/user.repository.ts
@@ -17,7 +17,6 @@ export class UserRepository extends BaseRepository<User> {
 
     public async findByEmail(email: string) {
         return super.findOne<Prisma.UserFindFirstArgs>({
-            ...this.select,
             where: { email },
         });
     }
